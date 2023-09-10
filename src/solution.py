554213164973d60bc70d00a23b7963e330585ecc
lindraaa <chancruz739@gmail.com>
2023-09-10 16:00:46 +0800

    commit na to

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,17 @@
 # Write a function that converts Celsius to Fahrenheit.
-def celsius_to_fahrenheit(_):
-    pass
+def celsius_to_fahrenheit(celcius, conv):
+    if conv == "fahrenheit":
+        fahrenheit = (celcius * 9/5) + 32
+        print("The fahrenheit is ", fahrenheit)
+      
+    elif conv =="kelvin":
+         kelvin = celcius + 273.15
+         print("The kelvin is ", kelvin)
+        
+    else:
+        print("Error")
+
+num = float(input("Enter the celcius: "))
+ans = input("Enter the conversion(fahrenheit,kelvin) ")
+
+celsius_to_fahrenheit(num,ans)
