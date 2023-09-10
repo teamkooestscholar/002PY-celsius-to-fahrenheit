26fee72e7e7778a34c3b27c3fe257a019e972b45
YoshiroSenpai <123640832+YoshiroTheDestroyer@users.noreply.github.com>
2023-09-11 00:25:28 +0800

    added a coded

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,26 @@
-# Write a function that converts Celsius to Fahrenheit.
-def celsius_to_fahrenheit(_):
-    pass
+def convert_temperature(value, from_scale, to_scale):
+    conversion_formulas = {
+        ('Celsius', 'Fahrenheit'): lambda x: (x * 9/5) + 32,
+        ('Celsius', 'Kelvin'): lambda x: x + 273.15,
+        ('Kelvin', 'Celsius'): lambda x: x - 273.15,
+        ('Kelvin', 'Fahrenheit'): lambda x: (x * 9/5) - 459.67,
+        ('Fahrenheit', 'Celsius'): lambda x: (x - 32) * 5/9,
+        ('Fahrenheit', 'Kelvin'): lambda x: ((x - 32) * 5/9) + 273.15, 
+        }
+        
+    if (from_scale, to_scale) in conversion_formulas:
+        conversion_func = conversion_formulas[(from_scale, to_scale)]
+        result = conversion_func(value)
+        return result
+    return None
+    
+value = float(input("Please enter the desired temperature: "))
+from_scale = input("Please enter the temperature you want to convert (Celsius, Fahrenheit, Kelvin): ").strip()
+to_scale = input("Please enter the temperature your converting to (Celsius, Fahrenheit, Kelvin): ").strip()
+
+result = convert_temperature(value, from_scale, to_scale)
+
+if result is not None:
+    print(f"{value} degrees {from_scale} is {result:.2f} degrees {to_scale}")
+else:
+    print("The Temperature you entered is invalid.")
