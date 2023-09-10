cb423f6dd41309cc99fe22109905f83e36efea7d
ysabelandreii <123624285+ysabelandreii@users.noreply.github.com>
2023-09-10 16:04:11 +0800

    Update solution.py
    Submission of solution.py for 002py - Convert Celsius to Fahrenheit

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,25 @@
-# Write a function that converts Celsius to Fahrenheit.
-def celsius_to_fahrenheit(_):
-    pass
+def convert_temperature(value, scale='Celsius'):
+    if scale == 'Celsius':
+        fahrenheit = (value * 9/5) + 32
+        return fahrenheit
+    elif scale == 'Kelvin':
+        fahrenheit = (value - 273.15) * 9/5 + 32
+        return fahrenheit
+    else:
+        return "Invalid scale. Supported scales are 'Celsius' and 'Kelvin'."
+
+def main():
+    try:
+        temperature = float(input("Enter the temperature value: "))
+        scale = input("Enter the temperature scale (Celsius or Kelvin): ").capitalize()
+
+        result = convert_temperature(temperature, scale)
+        if type(result) == float:
+            print(f"{temperature} {scale} is {result} Fahrenheit.")
+        else:
+            print(result)
+    except ValueError:
+        print("Invalid input. Please enter a valid numerical temperature value.")
+
+if __name__ == "__main__":
+    main()
