a6a9c38a85ba63b42010fd7275f62a6f206187ef
SparrowNado <123578310+SparrowNado@users.noreply.github.com>
2023-09-10 22:00:08 +0800

    increased readability of variable

--- src/solution.py
+++ src/solution.py
@@ -1,25 +1,24 @@
-# A function that converts Celsius to Fahrenheit.
-def convert_temperature(temperature, scale):
-    if scale.lower() == 'celsius':
+def convert_temperature(temperature, from_unit):
+    if from_unit.lower() == 'celsius':
         fahrenheit = (temperature * 9/5) + 32
         return fahrenheit
-    elif scale.lower() == 'kelvin':
+    elif from_unit.lower() == 'kelvin':
         celsius = temperature - 273.15
         fahrenheit = (celsius * 9/5) + 32
         return fahrenheit
     else:
-        raise ValueError("Unknown temperature scale. Please Restart the program ")
+        raise ValueError("Unsupported temperature unit")
 
 def main():
     try:
-        temperature = float(160) 
-        scale = input('celsius') 
+        input_temperature = float(input("Enter temperature: "))
+        input_unit = input("Enter current temperature unit (celsius/kelvin): ")
         
-        converted_temperature = convert_temperature(temperature, scale)
-        print(f"{temperature} {scale.capitalize()} is {converted_temperature:.2f} Fahrenheit.")
+        converted_temperature = convert_temperature(input_temperature, input_unit)
+        target_unit = 'Fahrenheit'
+        print(f"{input_temperature} {input_unit.capitalize()} is {converted_temperature:.2f} {target_unit}.")
     except ValueError as e:
         print(f"Error: {e}")
     
 if __name__ == "__main__":
     main()
-
