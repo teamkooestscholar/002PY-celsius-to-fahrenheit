42cc14ab2435fe054ce3044105ec93209b70cba0
Jonash Gania <jonashtiglao@gmail.com>
2023-09-10 15:30:18 +0800

    function to convert celsius/kelvin to fahrenheit

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,16 @@
-# Write a function that converts Celsius to Fahrenheit.
-def celsius_to_fahrenheit(_):
-    pass
+#function to convert celsius/kelvin to fahrenheit
+
+def celsius_to_fahrenheit(value, scale):
+    if scale == 'celsius':
+        celfah = round((value * (9/5) + 32), 2)
+        return f"{value} Celcius = {celfah} Fahrenheit"
+    elif scale == 'kelvin':
+        kelfah = round((value - 273.15) * (9/5) + 32, 2)
+        return f"{value} Kelvin = {kelfah} Fahrenheit"
+    else:
+        return "Wrong input choice"
+        
+user_scale = input("Which do you want to convert to fahrenheit? celsius or kelvin?: ").lower()
+user_value = float(input("Enter a value: "))
+
+print(celsius_to_fahrenheit(user_value, user_scale))
