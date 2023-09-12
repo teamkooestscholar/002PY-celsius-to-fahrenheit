b6acd88721f76cc664fb327796ea56c08fb760ab
DarrellBagsic <123875127+DarrellBagsic@users.noreply.github.com>
2023-09-12 22:43:42 +0800

    CFTemp

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,18 @@
-# Write a function that converts Celsius to Fahrenheit.
-def celsius_to_fahrenheit(_):
-    pass
+def convert_temperature(value, from_scale, to_scale):
+    if from_scale == 'C' and to_scale == 'F':
+        return (value * 9/5) + 32
+    elif from_scale == 'F' and to_scale == 'C':
+        return (value - 32) * 5/9
+    else:
+        return value  
+        
+if __name__ == "__main__":
+    try:
+        value = float(input("Enter temperature: "))
+        from_scale = input("Enter temperature scale [C for Celsius, F for Fahrenheit]: ").upper()
+        to_scale = input("Enter target temperature scale [C for Celsius, F for Fahrenheit]: ").upper()
+
+        converted_value = convert_temperature(value, from_scale, to_scale)
+        print(f"The converted temperature is: {converted_value} {to_scale}")
+    except ValueError:
+        print("Invalid input enter a valid numeric temperature value.")
