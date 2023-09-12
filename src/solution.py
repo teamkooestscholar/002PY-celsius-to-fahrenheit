45b877cc74d89ff281b80f402955e30dc8d5a4f6
Tsukkiz <123831791+Tsukkiz@users.noreply.github.com>
2023-09-12 22:27:15 +0800

    PY2Kenneth

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,20 @@
-# Write a function that converts Celsius to Fahrenheit.
-def celsius_to_fahrenheit(_):
-    pass
+def convert_temperature(value, scale='Celsius'):
+    if scale == 'C':
+        fahrenheit = (value * 9/5) + 32
+        return fahrenheit
+    elif scale == 'K':
+        fahrenheit = (value - 273.15) * 9/5 + 32
+        return fahrenheit
+    else:
+        return "Invalid temp. scale"
+
+if __name__ == "__main__":
+    temperature = float(input("Enter temp. value: "))
+    scale = input("Enter the temp. scale (C/K): ").strip()
+
+    result = convert_temperature(temperature, scale)
+    
+    if isinstance(result, str):
+        print(result)
+    else:
+        print(f"{temperature} {scale} is approximately {result} Fahrenheit")
