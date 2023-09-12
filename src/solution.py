8e74afe809fbbeb6d0fa7262125b574cc13a8a10
RaphaelSyrrusDNucum <144664548+RaphNucum@users.noreply.github.com>
2023-09-12 23:54:59 +0800

    raph

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,13 @@
-# Write a function that converts Celsius to Fahrenheit.
-def celsius_to_fahrenheit(_):
-    pass
+def celsius_to_fahrenheit(celsius):
+    # Formula to convert Celsius to Fahrenheit: F = (C * 9/5) + 32
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+# Input temperature in Celsius
+celsius_temperature = float(input("Enter temperature in Celsius: "))
+
+# Convert Celsius to Fahrenheit
+fahrenheit_temperature = celsius_to_fahrenheit(celsius_temperature)
+
+# Display the result
+print(f"{celsius_temperature} degrees Celsius is equal to {fahrenheit_temperature} degrees Fahrenheit.")
