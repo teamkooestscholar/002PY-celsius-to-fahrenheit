7a08c9088250409b12e37c1724db7ce202d5341d
Erhesvara <106751010+Mahesvaraaaa@users.noreply.github.com>
2023-09-12 20:56:46 +0800

    solution

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,18 @@
 # Write a function that converts Celsius to Fahrenheit.
-def celsius_to_fahrenheit(_):
-    pass
+def convert_cel_to_far(c):
+    """ converts celsius to Fahrenheit """
+    f = c * 9/5 + 32
+    return f
+
+
+def convert_far_to_cel(f):
+    """ converts fahrenheit to celsius """
+    c = (f - 32) * 5/9
+    return c
+
+
+user_cel = float(input("Enter a temperature in degrees F: "))
+print(f"{user_cel} degrees F = {convert_far_to_cel(user_cel):.2f} degrees C")
+
+user_far = float(input("Enter a temperature in degrees C: "))
+print(f"{user_far} degrees C = {convert_cel_to_far(user_far):.2f} degrees F")
