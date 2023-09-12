3cd2d5d9ec3a21cd9243c1415ca2947073b4951c
Cy12swe <104562626+Cy12swe@users.noreply.github.com>
2023-09-12 22:21:46 +0800

    Converts celsius to farenheit and kelvin

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,17 @@
-# Write a function that converts Celsius to Fahrenheit.
-def celsius_to_fahrenheit(_):
-    pass
+def convert_temperature(celsius, target_scale):
+    if target_scale == "fahrenheit":
+        fahrenheit = (celsius * 9/5) + 32
+        return fahrenheit
+    elif target_scale == "kelvin":
+        kelvin = celsius + 273.15
+        return kelvin
+    else:
+        return "Invalid target temperature scale"
+
+temperature_celsius = float(input("Enter the temperature in Celsius: "))
+
+temperature_fahrenheit = convert_temperature(temperature_celsius, "fahrenheit")
+print("Temperature in Fahrenheit:", temperature_fahrenheit)
+
+temperature_kelvin = convert_temperature(temperature_celsius, "kelvin")
+print("Temperature in Kelvin:", temperature_kelvin)
