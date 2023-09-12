9bc38c5b53be033f12264b5e2de009499e192d6b
mitaka3 <123709415+mitaka3@users.noreply.github.com>
2023-09-12 13:45:31 +0000

    justin

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,18 @@
-# Write a function that converts Celsius to Fahrenheit.
-def celsius_to_fahrenheit(_):
-    pass
+def celsius_to_fahrenheit(celsius, conversion):
+    if conversion == "fahrenheit":
+        fahrenheit = (celsius * 9/5) + 32
+        return fahrenheit
+    elif conversion == "kelvin":
+        kelvin = celsius + 273.15
+        return kelvin
+    else:
+        return "Error: Invalid conversion type"
+
+celsius = float(input("Enter the Celsius temperature: "))
+conversion_type = input("Enter the conversion type (fahrenheit or kelvin): ")
+
+result = celsius_to_fahrenheit(celsius, conversion_type)
+if isinstance(result, str):
+    print(result)
+else:
+    print("The", conversion_type, "is", result)
